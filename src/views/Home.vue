7d8c65cfa949a4b0f7fe96958eaf5ddfb39360a3
antonin <antonin@alors-la.fr>
2022-05-10 00:13:37 +0200

    :sparkles: Initialize VueJS

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home"><h1>the-game-of-cybersecurity</h1></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {},
+});
+</script>
